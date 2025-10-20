c0d783f81e3244f4fff109cf4f29d889a305cfd5
getabhishek321 <39920619+getabhishek321@users.noreply.github.com>
2025-10-20 23:07:53 +0530

    Add files via upload

--- 5. no if no but.py
+++ 5. no if no but.py
@@ -21,3 +21,28 @@ for na in names:
     else:
         print(f'this is {na}, give me any')
 
+#alien attack
+
+alien=['thor','hulk','loki','cap america']
+for al in alien:
+    if al=='thor':
+        print(f"did you just killed {al}, you earned 5 points for that")
+    elif al=='hulk':
+        print(f"did you just killed {al}, you earned 10 points for that")
+    elif al=='loki':
+        print(f"did you just killed {al}, you earned 100 points for that")
+    else:
+        print(f"bro did you just killed {al}, you earned 500 points for that")
+
+#alien attack 2
+
+alien='loki'
+
+if alien=='thor':
+        print(f"did you just killed thor, you earned 5 points for that")
+elif alien=='hulk':
+        print(f"did you just killed hulk, you earned 10 points for that")
+elif alien=='loki':
+        print(f"did you just killed loki, you earned 100 points for that")
+else:
+        print(f"bro did you just killed cap, you earned 500 points for that")
