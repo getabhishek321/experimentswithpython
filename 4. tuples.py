758850ab1e100e8fd94e0d8f2d08c1ee8d450473
getabhishek321 <39920619+getabhishek321@users.noreply.github.com>
2025-10-20 22:22:41 +0530

    Add files via upload

--- 4. tuples.py
+++ 4. tuples.py
@@ -7,3 +7,13 @@ print(dimensions[1])
 for dimension in dimensions:
     print(dimension)
 
+#you cannot change tuple, as you can change a list
+
+# dimensions[0]=('audi')
+# print(dimensions)
+# dimensions[0]=300
+# print(dimensions)
+
+# dimensions.append('audi')
+# print(dimensions)
+
