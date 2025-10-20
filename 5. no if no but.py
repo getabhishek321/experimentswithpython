734b2240a1cd6528adc4f8611e999c0de9da6002
getabhishek321 <39920619+getabhishek321@users.noreply.github.com>
2025-10-20 22:43:02 +0530

    Add files via upload

--- 5. no if no but.py
+++ 5. no if no but.py
@@ -5,3 +5,19 @@ if (a==2):
     print("hello")
 elif(a!=2):
     print("bye")
+
+#mixing lists with if statements
+
+names=['eenie','minnie','penguiny','mo']
+for na in names:
+    print(na)
+
+    if(na=="eenie"):
+        print(f'hi this is {na}, find me minnie')
+    elif(na=="minnie"):
+        print(f'hi this is {na}, find me penguiny')
+    elif(na=="penguiny"):
+        print(f'hi this is {na}, find me mo')
+    else:
+        print(f'this is {na}, give me any')
+
