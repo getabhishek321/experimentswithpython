ac5725acf6214c4093ebd1dc43c23082b956ec2a
getabhishek321 <39920619+getabhishek321@users.noreply.github.com>
2025-10-21 19:52:06 +0530

    Add files via upload

--- 6. its dictionary.py
+++ 6. its dictionary.py
@@ -4,3 +4,9 @@ print("hey lets learn dictionary \n")
 alien={'color':'green', 'point':5}
 print(alien['color'])
 print(alien['point'])
+
+
+#using values and pulling them in code.
+shot_alien=alien['color']
+earned_point=alien['point']
+print(f'you shot down {shot_alien} alien you earned {earned_point} points')
