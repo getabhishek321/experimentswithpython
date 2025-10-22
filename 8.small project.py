7fabac81a9e4aac1dcf3821e253841529f265c00
getabhishek321 <39920619+getabhishek321@users.noreply.github.com>
2025-10-23 01:55:33 +0530

    Add files via upload

--- 8.small project.py
+++ 8.small project.py
@@ -38,4 +38,35 @@ toppings={'butter':20,'ghee':30,'cheese':25}
 choice=input("i'd like to have: ")
 choice_list=choice.split(',')
 choice_list=[t.strip() for t in choice.split(',')]  
-print(choice_list)
+print("you choose: " ,choice_list)
+total1=0
+top=input(f"{name}, would you also like to add some toppings, say 'y' or 'n: ")
+if top=='y':
+   print("we have butter, ghee, cheese as toppings ")
+   top_choice=input("please choose toppings: ")
+   top_list=top_choice.split(',')
+   top_list=[k.strip() for k in top_choice.split(',')]
+   print("you choose: " ,top_list,"as toppings")
+   
+   for c in top_list:
+      if c in toppings:
+         print(f"{c} is available at {toppings[c]} rupeess")
+         total1+=toppings[c]
+      else:
+         print(f"{c} this isnt available")
+
+elif top=='n':
+   print("ok no problem, enjoy plain order")
+else:
+   print("wrong choice, try again")
+total = 0   # initialize before the loop
+for a in choice_list:
+    if a in menu:
+     print (f"{a} is available at {menu[a]}")
+     total += menu[a]    # only add price if available
+    else:
+       print(f"{a} this isnt available")
+
+grand=total1+total
+print(f"\nYour total bill is ₹{grand}")
+print(f"{name}, thank you for eating with us")
