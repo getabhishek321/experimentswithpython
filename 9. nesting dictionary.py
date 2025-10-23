7b0550148788391b09a9906dc655895c451c9cdf
getabhishek321 <39920619+getabhishek321@users.noreply.github.com>
2025-10-24 00:58:07 +0530

    Add files via upload

--- 9. nesting dictionary.py
+++ 9. nesting dictionary.py
@@ -1,9 +1,115 @@
-#simple nesting example
-alien_0 = {'color': 'green', 'points': 5}
-alien_1 = {'color': 'yellow', 'points': 10}
-alien_2 = {'color': 'red', 'points': 15}
+# #simple nesting example
+# alien_0 = {'color': 'green', 'points': 5}
+# alien_1 = {'color': 'yellow', 'points': 10}
+# alien_2 = {'color': 'red', 'points': 15}
 
-aliens=[alien_0,alien_1,alien_2]
+# aliens=[alien_0,alien_1,alien_2]
 
-for alien in aliens:
-    print(alien)
+# for alien in aliens:
+#     print(alien)
+
+# # please note that even if items are same, they will all be considered separate unique items by python
+
+# aliens=[]
+
+# for ali in range(30):
+#     newalien={'color': 'green', 'points': 5}
+#     aliens.append(newalien)
+
+# for alien in aliens[:5]:
+#     print(alien)
+
+# print(len(aliens))
+
+# # how about we change the first 3 aliens into a little fast moving yellow aliens.
+
+# for alien in aliens[:3]:
+#     if alien['color']=='green':
+#         alien['color']='yellow'
+#         alien['points']=10
+#         alien['speed']='medium'
+#     print(alien)
+
+# for alien in aliens[:6]:
+#     print(alien)
+
+# # A List in a Dictionary
+# pizza={
+#     "crust":"thick", "toppings":["cheese","macaroni","mushroom"]
+# }
+
+# #trying different ways to print and get output
+# print(f"so you ordered pizza with {pizza['crust']} crust and chose toppings: {pizza['toppings']}")
+# print(f"so you ordered pizza with {pizza['crust']} crust and chose toppings: {', '.join(pizza['toppings'])}")
+
+# print(f"You ordered a {pizza['crust']}-crust pizza "
+#  "with the following toppings:")
+
+# for topping in pizza['toppings']:
+#   print("\t" + topping)
+
+# # trying nesting multiple lists in dictionary
+
+# languez={
+#     "jen":["python","c","java"],
+#     "sofi":["c"],
+#     "avi":["java","ruby","python"]
+# }
+# for name, lang in languez.items():
+#     print(f"so {name}'s favorite languages are: {lang}") #this is common way to get lang items
+#     for langi in lang: #this is more refined way to do it
+#         print(f"{langi}")
+
+# # a dictionary in dictionary
+
+# users={
+#     "asingh":{
+#         "first":"abhi", "last":"singh","location":"ind"
+#     },
+#     "bsingh":{
+#         "first":"bikas","last":"singh","location":"us"
+#     }
+# }
+
+# for uname,uinfo in users.items():
+#     print(f"{uname}")
+#     una=(f"username is {uname} and first name is {uinfo['first']}, and last name is {uinfo['last']}")
+#     loc=uinfo["location"]
+
+#     print(f"{una.title()}")    
+#     print(f"location is {loc.title()}")
+
+
+    #practice nesting
+
+countr={
+        "india":{
+            "state":"up","cm":"yogi", "state":"assam", "cm":"himanta"
+        },
+        "amrika":{
+            "state":"mexico","cm":"sopra", "state":"penni", "cm":"nopra"
+        }
+    }
+
+for sc,st in countr.items():
+        print(f"country {sc}")
+        state=(f"{st['state']} in country {sc} has cm {st['cm']}")
+        print(f"{state}")
+
+#draft 2
+
+countr = {
+    "india": [
+        {"state": "up", "cm": "yogi"},
+        {"state": "assam", "cm": "himanta"}
+    ],
+    "amrika": [
+        {"state": "mexico", "cm": "sopra"},
+        {"state": "penni", "cm": "nopra"}
+    ]
+}
+
+for country, states in countr.items():
+    print(f"\nCountry: {country.title()}")
+    for info in states:
+        print(f"  State: {info['state'].title()} | CM: {info['cm'].title()}")
