c2554ebf219027b1df464dfc2b578c17cff2fbb5
getabhishek321 <39920619+getabhishek321@users.noreply.github.com>
2025-10-25 02:10:31 +0530

    Add files via upload

--- 11. user input and while loops.py
+++ 11. user input and while loops.py
@@ -10,3 +10,15 @@ age=input("whats your age: ")
 age=int(age)
 age=age+1
 print(age)
+
+
+ 
+#letting user end the program
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit':
+ message = input(prompt)
+ print(message)
+
